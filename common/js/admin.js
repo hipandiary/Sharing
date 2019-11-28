f2e5f2da9163b236a8f3842cc731dd36d3973858
hipan <18319154183@163.com>
2019-11-28 17:13:29 +0800

    完善注册判断

--- common/js/admin.js
+++ common/js/admin.js
@@ -7,6 +7,29 @@ $(function(){
         $(".box2").hide(700); 
         $(".box").show(700);   
    });
-
-  
+   
+   $(".getreg").click(function (e) { 
+        var user = $(".user").val();
+        var password = $(".password").val();
+        var password2 = $(".password2").val();
+        var email = $(".email").val();
+        var phone = $(".phone").val();
+        var aa = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+        if (user ==""||password == ""||password2 == ""||email==""||phone == "") {
+            alert("请完善你的注册信息")
+            return;
+        } 
+        if (password != password2) 
+            {
+                alert("请检查密码是否一致")
+                return;
+            }
+        if (!aa.test(email)) {
+            alert("请输入正确格式的邮箱")
+        } 
+        
+        if (phone.length<11) {
+           alert("请输入11位手机号")      
+        }
+   });    
 })
